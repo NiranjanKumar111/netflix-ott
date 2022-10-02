1ec8d5b46257cff0b622b8ae530145562f9b6410
ns04001@gmail.com <ns04001@gmail.com>
2022-10-03 01:48:16 +0530

    netflix

---
+++ src/Nav.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
+    // return (() => {
+    //     window.removeEventListener("scroll", transitionNavBar);
+    // });
+  }, []);
+
+  return (
+    <div className={`nav ${show && `nav__black`}`}>
+      <div className="nav__contents">
+        <img
+          className="nav__logo"
+          src="https://down.imgspng.com/download/0720/netflix_PNG26.png"
+          alt="netflix-logo"
+        />
+        <img
+          className="nav__avatar"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          alt="netflix-avatar"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB7QDnz3lz-11T9id5ypqyvXJWdvbiVupg",
+  authDomain: "netflix-ott-a7399.firebaseapp.com",
+  projectId: "netflix-ott-a7399",
+  storageBucket: "netflix-ott-a7399.appspot.com",
+  messagingSenderId: "44582632794",
+  appId: "1:44582632794:web:9581dbc794eb0a1037c763",
+};
